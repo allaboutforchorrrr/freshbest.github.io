aba58dddb9bf11fba56c19a01494ee4c929bd32d
allaboutforchorrrr <std22826@hwn.ac.th>
2026-02-13 16:14:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeimage(a){
+    var main = parent.frames['main'];
+    if (a == "a1"){main.getElementById('img').src='1.png';}
+    else if (a == "a2"){main.getElementById('img').src='2.png';}
+    else if (a == "a3"){main.getElementById('img').src='3.png';}
+    else if (a == "a4"){main.getElementById('img').src='4.png';}
+    else if (a == "a5"){main.getElementById('img').src='5.png';}
+    else if (a == "a6"){main.getElementById('img').src='6.png';}
+}
